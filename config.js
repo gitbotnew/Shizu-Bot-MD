9387cf5aa0e5a628debf3c1062c32d5a26c281c9
gitbotnew <115066920+gitbotnew@users.noreply.github.com>
2023-01-15 18:47:10 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,16 +8,16 @@ import axios from 'axios'
 import moment from 'moment-timezone'
 
 global.owner = [
-  ['34623442554', '👑 Shizu - Creador 👑', true],
-  ['34623442554']
+  ['923080613785', '👑 Shizu - Creador 👑', true],
+  ['923080613785']
 ] 
 global.suittag = ['34623442554'] 
 global.prems = ['34623442554'] 
 
 global.packname = '(≧▽≦)'
-global.author = '⍟𝗦𝗵𝗶𝘇𝘂 » 𝗛𝘂𝗯⍟'
-global.wm = '⍟𝗦𝗵𝗶𝘇𝘂 » 𝗕𝗼𝘁⍟ V5.3'
-global.igfg = '⍟𝗦𝗵𝗶𝘇𝘂 » 𝗕𝗼𝘁⍟'
+global.author = '⍟Devil » king⍟'
+global.wm = '⍟Devil » 𝗕𝗼𝘁⍟ V5.3'
+global.igfg = '⍟Devil » 𝗕𝗼𝘁⍟'
 global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
 
 global.keysZens = ['fiktod', 'c2459db922', 'BF39D349845E', '675e34de8a', '37CC845916', '0b917b905e6f', '6fb0eff124']
